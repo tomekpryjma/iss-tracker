23846cd668db744f3aaeac84562e3ab60d5f019c
Tomek <tompe97@tlen.pl>
2020-06-22 19:47:10 +0100

    Change structre of App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import './App.scss';
+import TrackingMap from './components/TrackingMap';
+import Astronauts from './components/Astronauts';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TrackingMap/>
+
+            <div className="container">
+                <Astronauts/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
